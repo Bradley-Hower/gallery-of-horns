005c9409ffed6c70fc5b5852828203c12fbd5148
Bradley Hower <bradleyhower@proton.me>
2023-09-19 13:56:26 -0700

    class-01

---
+++ Gallery of Horns/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './Component/Header';
+import Gallery from './Component/Gallery';
+import Footer from './Component/Footer'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Gallery />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
