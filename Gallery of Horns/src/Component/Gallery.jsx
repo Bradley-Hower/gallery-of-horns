c3ab6b11b418820241e5dd6269cd1c5d932ae668
Bradley Hower <bradleyhower@proton.me>
2023-09-29 14:56:46 -0700

    lab-04

--- Gallery of Horns/src/Component/Gallery.jsx
+++ Gallery of Horns/src/Component/Gallery.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import HornedBeastprofile from "./HornedBeastprofile";
 import HornedBeastValues from '../data/data.json';
-// import FilterForm from './FilterForm';
+import FilterForm from './FilterForm';
 import BeastDetails from './BeastDetails';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 class Gallery extends React.Component{
   constructor(){
     super();
     this.state = {
       detailedview: false,
+      showForm: false,
       beastslisted: HornedBeastValues,
       beast_title: null,
       beast_image_url: null,
@@ -20,9 +22,9 @@ class Gallery extends React.Component{
     }
   }
 
-  // toggleForm = () => {
-  //   this.setState({ showForm: !this.state.showForm});
-  // }
+  toggleForm = () => {
+    this.setState({ showForm: !this.state.showForm});
+  }
 
   toggleDetailedView = () => {
     this.setState({detailedview: !this.state.detailedview})
@@ -45,13 +47,20 @@ class Gallery extends React.Component{
   }
  
 
-  // filterhandler = () => {
-  //   return HornedBeastValues;
-  // }
+  handleFilter = (filterchoice, hornquantity) => {
+    if (filterchoice === 'horns'){
+      // this null needs to have filter attached
+      let filteredchoiceinput = HornedBeastValues.filter(element => element.horns > hornquantity);
+      this.setState({ beastslisted : filteredchoiceinput});
+    } else {
+      this.setState({ beastslisted : HornedBeastValues});
+    }
+  }
 
   render(){
     return(
       <div>
+        <Button onClick={this.toggleForm}>Filter</Button><br></br><br></br>
         <Row xs={1} md={4} className="g-4">
           {this.state.beastslisted.map((HornedBeast, idx) => (
             <Col key={idx}>
@@ -62,7 +71,7 @@ class Gallery extends React.Component{
           ))}
         </Row>
         
-        {/* <FilterForm showForm={this.state.showForm} toggleForm={this.toggleForm} handleFilter={this.handleFilter} /> */}
+        <FilterForm showForm={this.state.showForm} toggleForm={this.toggleForm} handleFilter={this.handleFilter} />
 
         <BeastDetails detailedview={this.state.detailedview} toggleDetailedView={this.toggleDetailedView} title={this.state.beast_title} image_url={this.state.beast_image_url} keyword={this.state.beast_keyword} description={this.state.beast_description} horns={this.state.beast_horns} selectbeast={this.selectbeast}/>
       </div>
